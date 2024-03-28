7bf484484be1eb90965cbf1d7b3df7102f393ccd
JBtheShadow <jbtheshadow@gmail.com>
2024-03-28 20:22:07 -0300

    JB - updating mod to .net 6

---
+++ TimeFreezesAtMidnight/Settings/ModConfig.cs
@@ -0,0 +1,16 @@
+﻿using TimeFreezesAtMidnight.Helpers;
+
+namespace TimeFreezesAtMidnight.Settings;
+internal sealed class ModConfig
+{
+    internal bool Enabled { get; set; } = true;
+
+    private int _timeFreezesAt = TimeHelper.DefaultValueTimeFreezesAt;
+    public int TimeFreezesAt
+    {
+        get => _timeFreezesAt;
+        set => _timeFreezesAt = TimeHelper.ClampTime(value);
+    }
+
+    public bool UseOldMethod { get; set; } = false;
+}
